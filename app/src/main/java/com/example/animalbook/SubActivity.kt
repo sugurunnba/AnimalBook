5fa50b9dafc534567560bf8099383a58466766be
sugurunnba <sugurunnba7@gmail.com>
2021-11-13 16:31:03 +0900

    finish

--- app/src/main/java/com/example/animalbook/SubActivity.kt
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -2,10 +2,55 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.animalbook.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubBinding
+//    タイトルフラグメントの初期化
+    private lateinit var title: TitleFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        activity_sub.xmlの中にあるlionButtonがクリックされた時に起動するメソッド
+        binding.lionButton.setOnClickListener {
+//            biginTransaction() = フラグメントマネージャーのインスタンスを取得
+            supportFragmentManager.beginTransaction().apply{
+//                LionFragmentをcontainerに配置する, replaceはフラグメントの入れ替えを行う
+                replace(R.id.container, LionFragment())
+//                バックスタックに保存, これを行うことで戻るボタンを押した時に１つ前の状態に戻すことができる
+                addToBackStack(null)
+//                フラグメントの処理を確定する
+                commit()
+            }
+        }
+        binding.hippoButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.container, HippoFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.giraffeButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.container, GiraffeFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+//        初期化したタイトルフラグメントを使用する
+        title = TitleFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.titleFrame, title)
+            commit()
+        }
+    }
+
+//    タイトルフラグメントに名前を入れる
+    override fun onResume(){
+        super.onResume()
+        title.setTitle("サブ画面")
     }
 }
--- app/src/main/java/com/example/animalbook/TitleFragment.kt
+++ app/src/main/java/com/example/animalbook/TitleFragment.kt
@@ -5,55 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.animalbook.databinding.FragmentTitleBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//Fragmentクラスを継承して作成
 class TitleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    val=定数, var=変数
+    private var _binding: FragmentTitleBinding? = null
+    private val binding
+        get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        生成されたバインディングクラスに含まれるinflateメソッドを呼び出す
+        _binding = FragmentTitleBinding.inflate(inflater, container, false)
+//        取得したルートビューをonCreateViewの戻り値として返す
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+//    取得したバインディングクラスをonDestoryViewメソッドで破棄する
+    override fun onDestroyView(){
+        super.onDestroyView()
+        _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TitleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TitleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+//    引数に指定した文字列をTextViewのtextプロパティに設定する
+    fun setTitle(title: String){
+        binding.titleText.text = title
     }
 }
